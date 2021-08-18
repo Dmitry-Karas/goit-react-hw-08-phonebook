2a9cd04beab50e63efd47fb5e9dd9039c1c9347d
Dmitry Karas <dmitrythekaras@gmail.com>
2021-08-18 18:12:03 +0300

    changed API name

--- src/redux/contacts/contactsOperations.js
+++ src/redux/contacts/contactsOperations.js
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { ContactsAPI } from "services/contactsApi";
+import { PhonebookAPI } from "services/phonebookApi";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/getContacts",
   async (_, { rejectWithValue }) => {
     try {
-      const contacts = await ContactsAPI.fetchContacts();
+      const contacts = await PhonebookAPI.fetchContacts();
 
       return contacts;
     } catch (error) {
@@ -18,7 +18,7 @@ export const addContact = createAsyncThunk(
   "contacts/addContact",
   async (contact, { rejectWithValue }) => {
     try {
-      const data = await ContactsAPI.addContact(contact);
+      const data = await PhonebookAPI.addContact(contact);
 
       return data;
     } catch (error) {
@@ -31,7 +31,7 @@ export const deleteContact = createAsyncThunk(
   "contacts/deleteContact",
   async (id, { rejectWithValue }) => {
     try {
-      await ContactsAPI.deleteContact(id);
+      await PhonebookAPI.deleteContact(id);
 
       return id;
     } catch (error) {
